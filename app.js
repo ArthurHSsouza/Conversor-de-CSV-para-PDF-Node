ab34ac90427f1d2e429b51910e282905d41ad645
ArthurHSsouza <g.ahss1980@gmail.com>
2021-02-11 14:28:42 -0300

    Adding readline-sync

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-var Reader = require("./Reader")
-var Processor = require("./Processor")
-var Table = require("./Table")
-var HTMLParser = require("./HTMLParser")
-var Writer = require('./PdfWriter')
-var HTMLWriter = require('./HTMLWriter')
+const Reader = require("./Reader");
+const Processor = require("./Processor");
+const Table = require("./Table");
+const HTMLParser = require("./HTMLParser");
+const HTMLWriter = require('./HTMLWriter');
+const readline = require('readline-sync');
 
-
- const reader = new Reader()
- const processor = new Processor()
- const writer = new HTMLWriter() 
+ const reader = new Reader();
+ const processor = new Processor();
+ const writer = new HTMLWriter();
 
 async function main(){
 
-var file  = await reader.read('./users.csv')
-var processedFile = processor.proccess(file);
-const table = new Table(processedFile)
-var tableHTML =  await HTMLParser.parser(table)
-var html = writer.write(Date.now()+".html",tableHTML)
-Writer.write(Date.now()+".pdf",tableHTML)
+    var path = readline.question("Digite o caminho do arquivo CSV: ");
+    while(path != "exit"){
+
+        try{
+            var file  = await reader.read(path);
+
+            var processedFile = processor.proccess(file);
+            const table = new Table(processedFile);
+            var tableHTML =  await HTMLParser.parser(table);
+            writer.write("./files/"+Date.now()+".html",tableHTML);
+            writer.write("./files/"+Date.now()+".pdf",tableHTML);
+        }catch(err){
+            console.log("Erro ao converter arquivo");
+        }
+    }
 }
 
-main()
+main();
 
 
 
